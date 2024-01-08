a1e75e2eef35d8e8003e81597def124808e6d1f3
ngcdat <datngocdo85@gmail.com>
2024-01-08 20:17:59 +0700

    NET

---
+++ Session1/Lab1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Lab1_1
+{
+    internal class Program
+    {
+        /// <summary>
+        /// in ra chữ C#
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    +++++++++             ++++     ++++");
+            Console.WriteLine("   ++++++++++++      ++++++++++++++++++++");
+            Console.WriteLine("  ++++     ++++     +++++++++++++++++++");
+            Console.WriteLine(" ++++                   ++++    ++++");
+            Console.WriteLine(" ++++                  ++++    ++++");
+            Console.WriteLine("  ++++     ++++    +++++++++++++++++++");
+            Console.WriteLine("   +++++++++++    +++++++++++++++++++");
+            Console.WriteLine("    +++++++++        ++++   ++++");
+        }
+    }
+}
---
+++ Session1/lab1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+
+namespace lab1_2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //khai báo biến 
+            int mnv = 01;
+            string name = "Tuong";
+            string ngaysinh = "16/04/2002";
+            string diachi = "Ha Noi";
+            int dienthoai = 0339474051;
+            string email = "dvtuong2002@gmail.com";
+
+            //inra 
+            Console.WriteLine("mnv : " + mnv);
+            Console.WriteLine("name : " + name);
+            Console.WriteLine("ngaysinh : " + ngaysinh);
+            Console.WriteLine("diachi : " + diachi);
+            Console.WriteLine("dienthoai : " + dienthoai);
+            Console.WriteLine("email : " + email);
+        }
+    }
+}
